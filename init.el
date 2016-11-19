6db7421b03cfbd9a8ffc7720bd9f3be3a1c97756
mdo <mdo@sapientia.lan>
2016-11-19 13:30:28 +0100

    Switched from paredit to smartparens.

--- init.el
+++ init.el
@@ -1,7 +1,5 @@
 ;;; init.el --- Configuration of Emacs
 ;;
-;; (c) 2000 - 2016 Arjen Wiersma
-;;
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -14,16 +12,13 @@
 
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "loader.org" init-dir))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (emmet-mode less-css-mode web-mode clj-refactor cider restclient magit company clojure-snippets yasnippet rainbow-delimiters highlight-parentheses paredit-everywhere paredit s htmlize markdown-mode cyberpunk-theme which-key bm use-package))))
+ '(org-support-shift-select t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
