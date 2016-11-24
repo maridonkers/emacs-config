e2ef553a422a1f9faa9d5734715ba49555c76124
mdo <mdo@sapientia.lan>
2016-11-24 19:30:08 +0100

    Revert init.el file.

--- init.el
+++ init.el
@@ -18,11 +18,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/.emacs.d/loader.org")))
- '(org-support-shift-select t)
- '(package-selected-packages
-   (quote
-    (bm use-package undo-tree smex smartparens restclient rainbow-delimiters projectile origami notmuch markdown-mode magit htmlize hl-sexp highlight-symbol highlight-parentheses expand-region cyberpunk-theme company clojure-snippets cider ag))))
+ '(org-support-shift-select t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
