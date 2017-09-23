3c1f663885d06de0072292b11b3e218cf676c849
mdo <git@photonsphere.org>
2017-09-23 19:59:15 +0200

    Cleaned up init.el, latest loader.org.

--- init.el
+++ init.el
@@ -12,28 +12,4 @@
 
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "loader.org" init-dir))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(delete-selection-mode nil)
- '(inhibit-startup-screen t)
- '(org-agenda-files nil)
- '(org-support-shift-select t)
- '(package-selected-packages
-   (quote
-    (emmet-mode point-undo notmuch ag undo-tree expand-region cider highlight-symbol hl-sexp restclient projectile magit origami company clojure-snippets yasnippet rainbow-delimiters highlight-parentheses smartparens s markdown-mode htmlize smex bm cyberpunk-theme use-package)))
- '(safe-local-variable-values
-   (quote
-    ((cider-cljs-lein-repl . "(do (dev) (go) (cljs-repl))")
-     (cider-refresh-after-fn . "reloaded.repl/resume")
-     (cider-refresh-before-fn . "reloaded.repl/suspend")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'upcase-region 'disabled nil)
-(put 'scroll-left 'disabled nil)
+
