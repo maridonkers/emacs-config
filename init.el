9c24e958fad3e400e0b01f0d7dd4bd2b4c2ba826
A.H.M. (Mari) Donkers <a.h.m.donkers@photonsphere.org>
2018-05-04 21:31:40 +0200

    Added scss-mode.

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
  '(highlight-symbol-idle-delay 0.5)
  '(package-selected-packages
    (quote
-    (company-web geiser julia-mode rust-mode edit-indirect clj-refactor which-key web-mode use-package undo-tree try smartparens sayid restclient rainbow-delimiters point-undo origami notmuch multiple-cursors move-dup markdown-mode magit hydra htmlize hl-sexp highlight-symbol highlight-parentheses goto-last-change expand-region emmet-mode cyberpunk-theme counsel-projectile company-php clojure-snippets bm auctex ag)))
+    (scss-mode company-web geiser julia-mode rust-mode edit-indirect clj-refactor which-key web-mode use-package undo-tree try smartparens sayid restclient rainbow-delimiters point-undo origami notmuch multiple-cursors move-dup markdown-mode magit hydra htmlize hl-sexp highlight-symbol highlight-parentheses goto-last-change expand-region emmet-mode cyberpunk-theme counsel-projectile company-php clojure-snippets bm auctex ag)))
  '(safe-local-variable-values
    (quote
     ((cider-cljs-lein-repl . "(do (dev) (go) (cljs-repl))")
